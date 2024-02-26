a1851553d33ec08e80c4b9ea1bf79205b0c1d681
lilfuz <lilfuz@gmail.com>
2024-02-27 00:36:20 +0300

    add full worked chat-gpt

--- backend.py
+++ backend.py
@@ -3,17 +3,44 @@
 #                SBR                #
 #               zzsxd               #
 #####################################
-import time
-from datetime import datetime
-
-
+import copy
+from freeGPT import Client
+from langdetect import detect
+import textwrap
 #####################################
+
 
 class TempUserData:
     def __init__(self):
+        super(TempUserData, self).__init__()
         self.__user_data = {}
+        self.__default = [None, None, None, []]
 
     def temp_data(self, user_id):
         if user_id not in self.__user_data.keys():
-            self.__user_data.update({user_id: [None, None, []]})
+            self.__user_data.update({user_id: [None, None, None, -1, [], []]}) # status, lang, main_question, counter_of_sub_quests, answers
         return self.__user_data
+
+    def clear_temp_data(self, user_id):
+        if user_id in self.__user_data.keys():
+            self.__user_data[user_id] = copy.deepcopy(self.__default)
+
+
+class ChatGpt:
+    def __init__(self):
+        super(ChatGpt, self).__init__()
+        self.__base_prompt = {'ru': {0: 'Составь наводящие вопросы помогающие составить бизнес план для ', 1: 'Составь бизнес план по ключевым особенностям '},
+                                'en': {0: 'Make up leading questions to help you make a business plan for ', 1: 'Make a business plan based on key features '}}
+
+    def detect_language(self, text):
+        return detect(text)
+
+    def gpt_query(self, prompt, lang, index):
+        if lang != 'ru':
+            lang = 'en'
+        answer = ''
+        try:
+            answer = Client.create_completion("gpt3", f'{self.__base_prompt[lang][index]}{prompt}')
+        except:
+            pass
+        return answer.split('\n')
--- main.py
+++ main.py
@@ -7,39 +7,42 @@ import os
 import platform
 import telebot
 from config_parser import ConfigParser
-from backend import TempUserData
+from backend import TempUserData, ChatGpt
 #####################################
 config_name = 'secrets.json'
 questions = ['What is your favorite color?', 'What is your favorite animal?', 'What is your favorite food?']
 #####################################
 
+
 def main():
     @bot.message_handler(commands=['start'])
     def send_question(message):
         bot.send_message(message.chat.id, "Hi! Enter a name for your business! I will help you with the development of a private business plan!")
-        temp_user_data.temp_data(message.chat.id)[message.chat.id][0] = 1
+        temp_user_data.temp_data(message.chat.id)[message.chat.id][0] = 0
 
     @bot.message_handler(content_types=['text'])
     def text(message):
         user_input = message.text
         user_id = message.chat.id
-        current_index = temp_user_data.temp_data(user_id)[user_id][2]
         user_current_action = temp_user_data.temp_data(user_id)[user_id][0]
-        if user_current_action == 1:
-            temp_user_data.temp_data(user_id)[user_id][1] = user_input
-            # отправка в бэкенд
-            temp_user_data.temp_data(user_id)[user_id][0] = 2
-        elif user_current_action == 2:
-            if current_index is None:
-                temp_user_data.temp_data(user_id)[user_id][2] = []
-                current_index = 0
-            temp_user_data.temp_data(user_id)[user_id][2].append(message.text)
-            current_index += 1
-            if current_index < len(questions):
-                bot.send_message(message.chat.id, questions[current_index])
+        if user_current_action == 0:
+            if temp_user_data.temp_data(user_id)[user_id][3] == -1:
+                temp_user_data.temp_data(user_id)[user_id][1] = chat_gpt.detect_language(user_input)
+                temp_user_data.temp_data(user_id)[user_id][2] = user_input
+                temp_user_data.temp_data(user_id)[user_id][4].extend(chat_gpt.gpt_query(user_input, temp_user_data.temp_data(user_id)[user_id][1], 0))
+                temp_user_data.temp_data(user_id)[user_id][3] = 0
+                bot.send_message(message.chat.id, temp_user_data.temp_data(user_id)[user_id][4][0])
             else:
-                bot.send_message(message.chat.id, "Вопросы закончились")
-                temp_user_data.temp_data(user_id)[user_id][2] = None
+                if temp_user_data.temp_data(user_id)[user_id][3] != len(temp_user_data.temp_data(user_id)[user_id][4])- 1:
+                    temp_user_data.temp_data(user_id)[user_id][5].append(user_input)
+                    temp_user_data.temp_data(user_id)[user_id][3] += 1
+                    index = temp_user_data.temp_data(user_id)[user_id][3]
+                    bot.send_message(message.chat.id, temp_user_data.temp_data(user_id)[user_id][4][index])
+                else:
+                    bot.send_message(message.chat.id, "Мы подготавливаем для Вас персональный план...")
+                    print(chat_gpt.gpt_query(f"{','.join(temp_user_data.temp_data(user_id)[user_id][5])} for {temp_user_data.temp_data(user_id)[user_id][2]}",
+                                       temp_user_data.temp_data(user_id)[user_id][1], 1))
+                    temp_user_data.temp_data(user_id)[user_id][2] = None
 
     bot.polling(none_stop=True)
 
@@ -47,7 +50,8 @@ def main():
 if '__main__' == __name__:
     os_type = platform.system()
     work_dir = os.path.dirname(os.path.realpath(__file__))
-    temp_user_data = TempUserData()
     config = ConfigParser(f'{work_dir}/{config_name}', os_type)
+    temp_user_data = TempUserData()
+    chat_gpt = ChatGpt()
     bot = telebot.TeleBot(config.get_config()['tg_api'])
     main()
