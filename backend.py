f41b71495f1a6a4d68b1970ed99830ad7ef960be
zzsxd <nikitacapkov06@gmail.com>
2024-02-28 22:43:58 +0800

    bebra

--- backend.py
+++ backend.py
@@ -75,18 +75,17 @@ class PDFCreate:
         pdf.cell(220, -100, company_name, 0, 1, 'C')
         pdf.set_text_color(0, 0, 0)
         pdf.set_font('Arial', size=15)
-
         # Добавляем ответы бота на 10 вопросов
         pdf.set_xy(0, 100)
-        lines = pdf.multi_cell(w=0, h=10, txt=text)
+        lines = pdf.multi_cell(w=0, h=10, txt=text[0])
 
-        # # Добавляем третью страницу в PDF файл
-        # pdf.add_page()
-        # pdf.image('ThirdPage.png', x=0, y=0, w=210, h=297)
-        # pdf.set_xy(0, 100)
-        # pdf.set_font('Arial', size=28)
-        # pdf.cell(210, 0, company_name, 0, 1, 'C')
-        # pdf.set_font('Arial', size=15)
+        # # Добавляем третью и тд страницу в PDF файл
+        for i in range(len(text) - 1):
+            pdf.add_page()
+            pdf.image('ThirdPage.png', x=0, y=0, w=210, h=297)
+            pdf.set_xy(0, 100)
+            pdf.set_font('Arial', size=28)
+            pdf.cell(210, 0, text[i + 1], 0, 1, 'C')
 
         # # Добавляем текст-рекомендацию
         # pdf.multi_cell(w=0, h=10, txt=text1)
