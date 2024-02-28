27936831c5d8ea9ecd3aab78e98b8355159dc2f4
lilfuz <lilfuz@gmail.com>
2024-02-28 16:10:06 +0300

    I won.

--- main.py
+++ main.py
@@ -19,39 +19,40 @@ config_name = 'secrets.json'
 def main():
     @bot.message_handler(commands=['start'])
     def send_question(message):
+        user_id = message.chat.id
         bot.send_message(message.chat.id, "Hi! Enter a name for your business! I will help you with the development of a private business plan!")
-        temp_user_data.temp_data(message.chat.id)[message.chat.id][0] = 0
+        temp_user_data.clear_temp_data(user_id)
 
     @bot.message_handler(content_types=['text'])
     def text(message):
         user_input = message.text
         user_id = message.chat.id
-        user_current_action = temp_user_data.temp_data(user_id)[user_id][0]
-        if user_current_action == 0:
-            if temp_user_data.temp_data(user_id)[user_id][3] == -1:
-                temp_user_data.temp_data(user_id)[user_id][1] = chat_gpt.detect_language(user_input)
-                temp_user_data.temp_data(user_id)[user_id][2] = user_input
-                temp_user_data.temp_data(user_id)[user_id][4].extend(chat_gpt.gpt_query(user_input, temp_user_data.temp_data(user_id)[user_id][1], 0))
-                temp_user_data.temp_data(user_id)[user_id][3] = 0
-                bot.send_message(message.chat.id, temp_user_data.temp_data(user_id)[user_id][4][0])
+        if temp_user_data.temp_data(user_id)[user_id][3] == -1:
+            temp_user_data.temp_data(user_id)[user_id][1] = chat_gpt.detect_language(user_input)
+            temp_user_data.temp_data(user_id)[user_id][2] = user_input
+            temp_user_data.temp_data(user_id)[user_id][4].extend(
+                chat_gpt.gpt_query(user_input, temp_user_data.temp_data(user_id)[user_id][1], 0))
+            temp_user_data.temp_data(user_id)[user_id][3] = 0
+            bot.send_message(message.chat.id, temp_user_data.temp_data(user_id)[user_id][4][0])
+        else:
+            if temp_user_data.temp_data(user_id)[user_id][3] != len(temp_user_data.temp_data(user_id)[user_id][4]) - 1:
+                temp_user_data.temp_data(user_id)[user_id][5].append(user_input)
+                temp_user_data.temp_data(user_id)[user_id][3] += 1
+                index = temp_user_data.temp_data(user_id)[user_id][3]
+                bot.send_message(message.chat.id, temp_user_data.temp_data(user_id)[user_id][4][index])
             else:
-                if temp_user_data.temp_data(user_id)[user_id][3] != len(temp_user_data.temp_data(user_id)[user_id][4])- 1:
-                    temp_user_data.temp_data(user_id)[user_id][5].append(user_input)
-                    temp_user_data.temp_data(user_id)[user_id][3] += 1
-                    index = temp_user_data.temp_data(user_id)[user_id][3]
-                    bot.send_message(message.chat.id, temp_user_data.temp_data(user_id)[user_id][4][index])
-                else:
-                    bot.send_message(message.chat.id, "Мы подготавливаем для Вас персональный план...")
-                    answer = chat_gpt.gpt_query(f"{','.join(temp_user_data.temp_data(user_id)[user_id][5])} for {temp_user_data.temp_data(user_id)[user_id][2]}",
-                                       temp_user_data.temp_data(user_id)[user_id][1], 1)
-                    pdf_creator.create_pdf(temp_user_data.temp_data(user_id)[user_id][2], '\n'.join(answer))
-                    with open("plan.pdf", "rb") as misc:
-                        obj = BytesIO(misc.read())
-                        obj.name = 'plan.pdf'
-                    bot.send_document(user_id, obj)
-                    os.remove('plan.pdf')
-                    temp_user_data.temp_data(message.chat.id)[message.chat.id][0] = None
-                    temp_user_data.clear_temp_data(user_id)
+                bot.send_message(message.chat.id, "Мы подготавливаем для Вас персональный план...")
+                answer = chat_gpt.gpt_query(
+                    f"{','.join(temp_user_data.temp_data(user_id)[user_id][5])} for {temp_user_data.temp_data(user_id)[user_id][2]}",
+                    temp_user_data.temp_data(user_id)[user_id][1], 1)
+                pdf_creator.create_pdf(temp_user_data.temp_data(user_id)[user_id][2], '\n'.join(answer))
+                with open("plan.pdf", "rb") as misc:
+                    obj = BytesIO(misc.read())
+                    obj.name = 'plan.pdf'
+                bot.send_document(user_id, obj)
+                os.remove('plan.pdf')
+                temp_user_data.clear_temp_data(user_id)
+
 
     bot.polling(none_stop=True)
 
